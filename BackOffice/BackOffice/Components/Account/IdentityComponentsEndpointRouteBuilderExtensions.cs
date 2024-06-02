db82548003f6cf0e182ba2bcc7fabe478f330ca4
Sotis11K <89807156+Sotis11K@users.noreply.github.com>
2024-06-02 20:36:52 +0200

    finished

--- BackOffice/BackOffice/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ BackOffice/BackOffice/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -27,17 +27,11 @@ namespace Microsoft.AspNetCore.Routing
                 [FromForm] string provider,
                 [FromForm] string returnUrl) =>
             {
-                IEnumerable<KeyValuePair<string, StringValues>> query = [
-                    new("ReturnUrl", returnUrl),
-                    new("Action", ExternalLogin.LoginCallbackAction)];
+               
 
-                var redirectUrl = UriHelper.BuildRelative(
-                    context.Request.PathBase,
-                    "/Account/ExternalLogin",
-                    QueryString.Create(query));
+                
 
-                var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-                return TypedResults.Challenge(properties, [provider]);
+                
             });
 
             accountGroup.MapPost("/Logout", async (
@@ -59,12 +53,9 @@ namespace Microsoft.AspNetCore.Routing
                 // Clear the existing external cookie to ensure a clean login process
                 await context.SignOutAsync(IdentityConstants.ExternalScheme);
 
-                var redirectUrl = UriHelper.BuildRelative(
-                    context.Request.PathBase,
-                    "/Account/Manage/ExternalLogins",
-                    QueryString.Create("Action", ExternalLogins.LinkLoginCallbackAction));
+                
 
-                var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl, signInManager.UserManager.GetUserId(context.User));
+                var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, signInManager.UserManager.GetUserId(context.User));
                 return TypedResults.Challenge(properties, [provider]);
             });
 
